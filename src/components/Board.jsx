736d2407b32a86deafd1a6b5fa042822583538f4
Fata Sefer <sefer.fata@gmail.com>
2022-12-01 22:53:11 +0100

    feat: write initial handling click logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Square } from "./Square";
 
-export const Board = ({ squares, onClick }) => {
+export const Board = ({ onClick }) => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  };
+
+  console.log(squares);
   return (
     <div className="grid grid-cols-3 bg-rose-500 rounded-2xl">
       {squares.map((square, i) => (
-        <Square key={i} value={squares[i]} onClick={() => onClick(i)} />
+        <Square key={i} value={squares[i]} handleClick={() => handleClick(i)} />
       ))}
     </div>
   );
