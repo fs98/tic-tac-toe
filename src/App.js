5cb1e78cf6051785c0ef19c0d2bf56bf624e06f3
Fata Sefer <sefer.fata@gmail.com>
2022-12-16 15:41:23 +0100

    refactor: move status displaying element to Board component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { Board } from "./components/Board";
 
-const status = "Next player is X";
-const moves = ["1", "2"];
-
 const App = () => (
   <>
     <header></header>
@@ -12,10 +9,6 @@ const App = () => (
           Tic <span className="text-rose-500">Tac</span> Toe
         </h1>
         <Board />
-        <div className="text-center px-10">
-          <p>{status}</p>
-          <ul>{moves}</ul>
-        </div>
         <button className="px-4 py-2 rounded-lg font-bold bg-gradient-to-b from-amber-300 to-amber-400 hover:from-amber-400 hover:to-amber-500">
           New Game
         </button>
