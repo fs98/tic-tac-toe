218eb346462662bcf627bc05965cc935896ac62a
Fata Sefer <sefer.fata@gmail.com>
2022-12-16 15:40:42 +0100

    css: style the game on smaller screens

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 export const Square = ({ value, handleClick }) => {
   return (
     <button
-      className="px-12 py-7 text-center font-bold align-middle text-3xl md:text-7xl border text-amber-400"
+      className="px-6 py-1 sm:px-12 sm:py-7 h-20 w-20 sm:h-36 sm:w-36 text-center font-bold align-middle text-3xl sm:text-7xl border text-amber-400"
       onClick={() => handleClick()}
     >
       {value}
