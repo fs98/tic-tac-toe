19fbc7d06a321e1080316ceb599ce7c8eb676251
Fata Sefer <sefer.fata@gmail.com>
2022-12-16 15:41:42 +0100

    feat: finish the logic of the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,67 @@
 import React, { useState } from "react";
 import { Square } from "./Square";
 
-export const Board = ({ onClick }) => {
+export const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    if (!squares.includes(null)) {
+      status = "It's a draw!";
+    } else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   };
 
-  console.log(squares);
   return (
-    <div className="grid grid-cols-3 bg-rose-500 rounded-2xl">
-      {squares.map((square, i) => (
-        <Square key={i} value={squares[i]} handleClick={() => handleClick(i)} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-3 bg-rose-500 rounded-2xl">
+        {squares.map((square, i) => (
+          <Square
+            key={i}
+            value={squares[i]}
+            handleClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
+      <div className="text-center px-10">
+        <p>{status}</p>
+      </div>
+    </>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
